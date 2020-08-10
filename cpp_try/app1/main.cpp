eb9e34e9d89b385836115c55327aa5e770f55f10
XUXIANG-PC\XX <374591248@qq.com>
2020-08-10 23:39:20 +0800

    宏兼容修正

--- cpp_try/app1/main.cpp
+++ cpp_try/app1/main.cpp
@@ -6,12 +6,13 @@
 #include <chrono>
 #include <thread>
 #include <csignal>
-#include <unistd.h>
-#include<sys/types.h>
-#include <sys/stat.h>
 
 #if defined(WIN)
 #include <WinSock2.h>
+#else
+#include <unistd.h>
+#include<sys/types.h>
+#include <sys/stat.h>
 #endif
 
 using namespace Net;
@@ -26,35 +27,55 @@ void OnSignal(int signum)
 	is_run = false;
 }
 
-int main()
+//启动守护进程
+void start_daemon()
 {
-
+#if !defined(WIN)
 	//1. 创建子进程，每个进程都会返回一个数字，父进程返回子进程的pid，子进程返回0
 	int ret_code = fork();
 	if (ret_code < 0)
 	{
-		return 1;
+		exit(1);
 	}
 	else if (ret_code>0)
 	{
 		//父进程退出
-		return 0;
+		exit(0);
 	}
-	
+
 	//2. 令子进程与父进程的会话组和进程组脱离，令子进程不受终端关闭影响
 	setsid();
 
+	////3. 如果担心目前进程会打开终端并受终端影响，可再fork一次，新进程将不会是会话组长，因此无法打开新终端。
+	//// PS:这一步不是必须的。
+	//ret_code = fork();
+	//if (ret_code < 0)
+	//{
+	//	exit(1);
+	//}
+	//else if (ret_code>0)
+	//{
+	//	exit(0);
+	//}
+
+	////4. 改变进程的工作目录
+	//chdir("/");
 
 	//5. 设置进程的文件操作权限，使进程拥有最大权限
 	umask(0);
-	
+
 	//6. 将父进程打开的文件描述符关闭
 	//getdtablesize():获取描述符表的大小
-	for(int fd = 0;fd< getdtablesize();fd++)
+	for (int fd = 0; fd< getdtablesize(); fd++)
 	{
 		close(fd);
 	}
+#endif
+}
 
+int main()
+{
+	start_daemon();
 
 #if defined(WIN)
 	//初始化网络
